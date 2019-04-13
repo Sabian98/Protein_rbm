2cb3b11f98a60dbe63bd4d88e6e10eb9704d91ee
Sabian98 <taseefriut@gmail.com>
2019-04-12 22:36:03 -0400

    First round of training done

---
+++ prot2.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pca
+import numpy as np
+import xml.etree.ElementTree as ET
+import Bio.SVDSuperimposer
+from Bio.SVDSuperimposer import SVDSuperimposer
+from math import sqrt
+from numpy import array, dot
+import random
+import operator
+import os
+import sys
+import pickle
+from sklearn.decomposition import PCA
+import math
+import scipy.io
+from sklearn.model_selection import train_test_split
+from keras.layers import Input, Dense
+from keras.models import Model
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras import regularizers
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import minmax_scale
+from numpy.random import seed
+from sklearn.preprocessing import StandardScaler
+
+
+# In[2]:
+
+def data():
+	coordinate_file = 'onedtja.txt'
+	aligned_dict, ref = pca.align(coordinate_file)
+#Read the Energy List
+
+	energylist = []
+	#read the energy file
+	with open('onedtja_energy.txt', 'r') as f:
+		for line in f:
+			line = float(line.strip())
+			energylist.append(line)          
+	energyarray = np.array(energylist).reshape(-1,1)
+	# print(energyarray.shape)
+
+
+# In[4]:
+
+
+#converting Aligned dictionary to Aligned Array
+	alignedlist=[]
+	for key, val in aligned_dict.items():
+		alignedlist.append(val)
+	alignedArray = np.array(alignedlist)
+	centered_data, mean = pca.center(alignedArray)
+	centered_data=centered_data.T
+
+
+
+#print(test_scaled.shape)
+	scaler = StandardScaler()
+	scaler.fit(centered_data)
+	X_scaled = scaler.transform(centered_data)
+# print(X_scaled.shape)
+# print(X_scaled)
+
+
+
+	initialpc_and_energy = np.concatenate((X_scaled, energyarray), axis = 1)
+
+
+
+# #Instead of All data, here we are just taking first 1000 data
+	X = initialpc_and_energy[0:3000,:]
+	train, test = train_test_split(X,train_size=0.6, test_size=0.4, random_state=42)
+
+
+# # separate Energy from train data 
+	train_after_enegy, EnergyAfterTrain = train[:, 0:222], train[:, 222:223]
+
+
+# # separate Energy from test data
+	test_after_enegy, EnergyAfterTest = test[:, 0:222], test[:, 222:223]
+
+
+
+	train_x = np.array(train_after_enegy)
+	test_x = np.array(test_after_enegy)
+	return train_x,test_x
+
+
---
+++ train.py
@@ -0,0 +1,94 @@
+import os
+from rbm import RBM
+from au import AutoEncoder
+import tensorflow as tf
+from utilsnn import get_random_block_from_data
+import matplotlib.pyplot as plt
+# from tfrbm import GBRBM
+import prot2
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', 'F:\GMU\Spring 19\CS 701\protein\protein_rbm', 'Directory for storing data')
+flags.DEFINE_integer('epochs',1000, 'The number of training epochs')
+flags.DEFINE_integer('batchsize', 64, 'The batch size')
+flags.DEFINE_boolean('restore_rbm', False, 'Whether to restore the RBM weights or not.')
+
+
+train_x,test_x=prot2.data()  #data imported
+
+# ensure output dir exists
+if not os.path.isdir('out'):
+  os.mkdir('out')
+
+# RBMs
+rbmobject1 = RBM(222, 128, ['rbmw1', 'rbvb1', 'rbmhb1'], 0.3)
+rbmobject2 = RBM(128, 64, ['rbmw2', 'rbvb2', 'rbmhb2'], 0.3)
+rbmobject3 = RBM(64, 32, ['rbmw3', 'rbvb3', 'rbmhb3'], 0.3)
+rbmobject4 = RBM(32, 2,   ['rbmw4', 'rbvb4', 'rbmhb4'], 0.3)
+
+if FLAGS.restore_rbm:
+  rbmobject1.restore_weights('./out/rbmw1.chp')
+  rbmobject2.restore_weights('./out/rbmw2.chp')
+  rbmobject3.restore_weights('./out/rbmw3.chp')
+  rbmobject4.restore_weights('./out/rbmw4.chp')
+
+# Autoencoder
+autoencoder = AutoEncoder(222, [128, 64, 32, 2], [['rbmw1', 'rbmhb1'],
+                                                    ['rbmw2', 'rbmhb2'],
+                                                    ['rbmw3', 'rbmhb3'],
+                                                    ['rbmw4', 'rbmhb4']], tied_weights=False)
+
+iterations = int(len(train_x) / FLAGS.batchsize)
+
+
+# Train First RBM
+print('first rbm')
+for i in range(FLAGS.epochs):
+  for j in range(iterations):
+    # batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batchsize)
+    data_xs=get_random_block_from_data(train_x,64)
+    rbmobject1.partial_fit(data_xs)
+  #print(rbmobject1.compute_cost(train_x))
+  # show_image("out/1rbm.jpg", rbmobject1.n_w, (28, 28), (30, 30))
+rbmobject1.save_weights('./out/rbmw1.chp')
+
+print('second rbm')
+for i in range(FLAGS.epochs):
+  for j in range(iterations):
+    # batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batchsize)
+    # Transform features with first rbm for second rbm
+    data_xs = get_random_block_from_data(train_x, 64)
+    batch_xs = rbmobject1.transform(data_xs)
+    rbmobject2.partial_fit(batch_xs)
+  #print(rbmobject2.compute_cost(rbmobject1.transform(train_x)))
+  # show_image("out/2rbm.jpg", rbmobject2.n_w, (30, 30), (25, 20))
+rbmobject2.save_weights('./out/rbmw2.chp')
+
+print('third rbm')
+for i in range(FLAGS.epochs):
+  for j in range(iterations):
+    # Transform features
+    data_xs = get_random_block_from_data(train_x, 64)
+    batch_xs = rbmobject1.transform(data_xs)
+    batch_xs = rbmobject2.transform(batch_xs)
+    rbmobject3.partial_fit(batch_xs)
+  #print(rbmobject3.compute_cost(rbmobject2.transform(rbmobject1.transform(train_x))))
+  #show_image("out/3rbm.jpg", rbmobject3.n_w, (25, 20), (25, 10))
+rbmobject3.save_weights('./out/rbmw3.chp')
+#
+# print("second training done")
+# Train Third RBM
+print('fourth rbm')
+for i in range(FLAGS.epochs):
+  for j in range(iterations):
+    data_xs = get_random_block_from_data(train_x, 64)
+    # Transform features
+    batch_xs = rbmobject1.transform(data_xs)
+    batch_xs = rbmobject2.transform(batch_xs)
+    batch_xs = rbmobject3.transform(batch_xs)
+    rbmobject4.partial_fit(batch_xs)
+  print(rbmobject4.compute_cost(rbmobject3.transform(rbmobject2.transform(rbmobject1.transform(train_x)))))
+rbmobject4.save_weights('./out/rbmw4.chp')
+
+
